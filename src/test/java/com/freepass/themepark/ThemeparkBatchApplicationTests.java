299436416d3a7f87574c9eda760362163f8cb90e
wisekim <lwisekiml@naver.com>
2024-02-16 18:41:10 +0900

    first commit

---
+++ src/test/java/com/freepass/themepark/ThemeparkBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freepass.themepark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeparkBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
